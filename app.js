9144c3e688b90115baa5d43153634225581b3676
Rishabh Baid <57484495+Rishabh2012329@users.noreply.github.com>
2021-01-26 12:26:35 +0530

    change

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb+srv://Rishabh:rishabh231@cluster0.dcbjl.mongodb.net/Cluster0?retryWrites=true&w=majority',()=>{
+mongoose.connect('',()=>{
     console.log("connected")
 })
 
